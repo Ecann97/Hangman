7bfc6a7b58fd92727ea3b24588adf178e121ad7d
Evan Cann <sEvanCann@yccc.edu>
2018-10-10 20:09:36 -0400

    first commit

---
+++ src/Game.java
@@ -0,0 +1,59 @@
+import java.util.*;
+public class Game {
+private String hint;
+private String word;
+private String guess="";
+
+
+public String getHint() {
+	return hint;
+}
+public void setHint(String hint) {
+	this.hint = hint;
+}
+public String getWord() {
+	return word;
+}
+public void setWord(String word) {
+	this.word = word;
+}
+public String getGuess() {
+	return guess;
+}
+public void setGuess(String guess) {
+	this.guess += guess;
+}
+
+public boolean isSolved()
+{
+	return generateMask().equals(word);
+}
+public String generateMask()
+{
+
+	String mask = "";
+	for(int i = 0; i < word.length(); i++)
+	{
+		String currentChar = String.valueOf(word.charAt(i));
+		if(guess.contains(currentChar))
+		{
+			mask += currentChar;
+		}
+		else
+		{
+			mask += "*";
+		}
+		}
+		
+	
+	return mask;
+}
+
+public void getGuesses()
+{
+
+}
+}
+
+
+
--- src/Hangclass.java
+++ src/Hangclass.java
@@ -1,19 +1,22 @@
 import java.util.*; 
 public class Hangclass {
 	//I need some words to seed into the program
-	static String s1 = "cat";
+	
 	//static String s2 = "Their";
 	//static String s3 = "Museum";
 	//static String s4 = "Rough";
 	//static String s5 = "House";
-	 
+	static Game g = new Game();
 	
 	
 	public static void main(String[] args)
 	{
+		g.setHint("Meow");
+		g.setWord("Cat");
+		 
 		int c = 0;
 	@SuppressWarnings("resource")
-	Scanner console = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);
 	
 	//Random r = new Random(4);
 	// ^^ this can be used to add randomness to game.
@@ -21,29 +24,52 @@ public class Hangclass {
 	//^^ can be used to randomly generate a word without use of arrays.
 	
 	//This example will be using the s1 string marked as "cat"
-		System.out.println("*** " + "3 letter word  ");
+		
 	//Game ends when the number of guesses is at the word length
-		for(int i=0; i <= s1.length(); i++)
+	while(c<g.getWord().length())
+	{
+		
+		for(int i=0; i <g.getWord().length(); i++)
+		
 		{
-			System.out.println("Guess a letter ");
+			System.out.println(" Guess a letter ");
+			System.out.println("Hint " + g.getHint());
+			System.out.println("Mask " + g.generateMask());
 			@SuppressWarnings("unused")
-			String g = console.next();
+			String userGuess = sc.nextLine();
+			g.setGuess(userGuess);
 			
-			
-				if(s1.contains(g))
+			if(c<g.getWord().length())
+			{
+				if(g.getWord().contains(userGuess))
+				
 				{
-					System.out.println("Correct.  Guess another letter");
+					System.out.println("Correct.  You have " + (g.getWord().length()-c) + " guesses left.");
 				}
 				else
 				{
-					c = c+1;
-					System.out.println("Incorrect.  You have " + (s1.length()-c) + " " + "guesses left.  Guess another letter.");
+					c+=1;
+					System.out.println("Incorrect.  You have " + (g.getWord().length()-c) + " guesses left. ");
 				}
+		
+			}
+			else
+			{
+				System.out.println("Game Over");
+			}
 				
 				
 		}
+	}
 	
+		
+		
+			
+		
+		
 	
 	}
 
 }
+
+
